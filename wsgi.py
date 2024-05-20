f31d3dbecce947b9244e81c58ff8546db38c4dff
Felix Blanke <felixblanke@uni-bonn.de>
2024-05-20 02:57:11 +0200

    Add post barebones

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,15 @@
 import json
 
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 app.config.from_file("config.json", load=json.load)
 
 @app.route("/<poll_name>", methods=["POST"])
 def post_result(poll_name: str):
-    ...
+    # TODO: Process request
+    print(request.form)
+    return render_template("success.html")
 
 
 @app.route("/<poll_name>", methods=["GET"])
