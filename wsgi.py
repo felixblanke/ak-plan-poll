dafd4ec30df7c4cd5e4e2ad6665b90e2d52aa0c7
Felix Blanke <felixblanke@uni-bonn.de>
2024-05-20 03:55:33 +0200

    Fix typos and formatting

--- wsgi.py
+++ wsgi.py
@@ -5,10 +5,9 @@ from flask import Flask, render_template, url_for, request
 app = Flask(__name__)
 app.config.from_file("config.json", load=json.load)
 
-def dumm_get_aks(poll_name: str) -> list[str]:
-    ak_dict = {
-        "koma90-test": ["AK Berufungshandbuch", "AK Testwurst"]
-    }
+
+def dummy_get_aks(poll_name: str) -> list[str]:
+    ak_dict = {"koma90-test": ["AK Berufungshandbuch", "AK Testwurst"]}
     return ak_dict.get(poll_name, None)
 
 
@@ -18,13 +17,15 @@ def post_result(poll_name: str):
     print(request.form)
     return render_template("success.html")
 
+
 @app.route("/", methods=["GET"])
 def landing_page():
     return render_template("unknown.html")
 
+
 @app.route("/<poll_name>", methods=["GET"])
 def get_form(poll_name: str):
-    ak_list = dumm_get_aks(poll_name=poll_name)
+    ak_list = dummy_get_aks(poll_name=poll_name)
     if ak_list is None:
         return render_template("unknown.html")
     else:
