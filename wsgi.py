1fd1b3e06b8b7f730ccc7f8cc5092150e7264504
Felix Blanke <felixblanke@uni-bonn.de>
2024-05-20 03:05:03 +0200

    Add dummy ak list for now

--- wsgi.py
+++ wsgi.py
@@ -5,6 +5,10 @@ from flask import Flask, render_template, url_for, request
 app = Flask(__name__)
 app.config.from_file("config.json", load=json.load)
 
+def dumm_get_aks(poll_name: str) -> list[str]:
+    return ["AK Berufungshandbuch", "AK Testwurst"]
+
+
 @app.route("/<poll_name>", methods=["POST"])
 def post_result(poll_name: str):
     # TODO: Process request
@@ -14,4 +18,4 @@ def post_result(poll_name: str):
 
 @app.route("/<poll_name>", methods=["GET"])
 def get_form(poll_name: str):
-    return render_template("poll.html", poll_name=poll_name)
+    return render_template("poll.html", poll_name=poll_name, ak_list=dumm_get_aks(poll_name=poll_name))
