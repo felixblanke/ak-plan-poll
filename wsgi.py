74d1f4ff5744c9972f0543392b0a3c2264219bf9
Felix Blanke <felixblanke@uni-bonn.de>
2024-05-20 03:27:28 +0200

    Add unknown page

--- wsgi.py
+++ wsgi.py
@@ -6,7 +6,10 @@ app = Flask(__name__)
 app.config.from_file("config.json", load=json.load)
 
 def dumm_get_aks(poll_name: str) -> list[str]:
-    return ["AK Berufungshandbuch", "AK Testwurst"]
+    ak_dict = {
+        "koma90": ["AK Berufungshandbuch", "AK Testwurst"]
+    }
+    return ak_dict.get(poll_name, None)
 
 
 @app.route("/<poll_name>", methods=["POST"])
@@ -15,7 +18,14 @@ def post_result(poll_name: str):
     print(request.form)
     return render_template("success.html")
 
+@app.route("/", methods=["GET"])
+def landing_page():
+    return render_template("unknown.html")
 
 @app.route("/<poll_name>", methods=["GET"])
 def get_form(poll_name: str):
-    return render_template("poll.html", poll_name=poll_name, ak_list=dumm_get_aks(poll_name=poll_name))
+    ak_list = dumm_get_aks(poll_name=poll_name)
+    if ak_list is None:
+        return render_template("unknown.html")
+    else:
+        return render_template("poll.html", poll_name=poll_name, ak_list=ak_list)
