f58a1b2f15e96ab3067d0f628478d21bdc57d288
Felix Blanke <felixblanke@uni-bonn.de>
2024-05-20 04:28:15 +0200

    Read AKs from file

--- wsgi.py
+++ wsgi.py
@@ -3,13 +3,10 @@ import json
 from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
-app.config.from_file("config.json", load=json.load)
+# app.config.from_file("config.json", load=json.load)
 
-
-def dummy_get_aks(poll_name: str) -> list[str]:
-    ak_dict = {"koma90-test": ["AK Berufungshandbuch", "AK Testwurst"]}
-    return ak_dict.get(poll_name, None)
-
+with open("aks.json", "r") as ff:
+    ak_dict = json.load(ff)
 
 @app.route("/<poll_name>", methods=["POST"])
 def post_result(poll_name: str):
@@ -47,8 +44,7 @@ def landing_page():
 
 @app.route("/<poll_name>", methods=["GET"])
 def get_form(poll_name: str):
-    ak_list = dummy_get_aks(poll_name=poll_name)
-    if ak_list is None:
-        return render_template("unknown.html")
-    else:
+    if (ak_list := ak_dict.get(poll_name, None)) is not None:
         return render_template("poll.html", poll_name=poll_name, ak_list=ak_list)
+    else:
+        return render_template("unknown.html")
