8d8bfeced454c93cf8b7e811778c379ea7ff71ff
Felix Blanke <felixblanke@uni-bonn.de>
2024-05-26 21:51:52 +0200

    Migrate wiki import to script

---
+++ import_from_wiki.py
@@ -0,0 +1,33 @@
+import json
+
+import fire
+import numpy as np
+import pandas as pd
+
+from wsgi import write_ak_data
+
+
+def df_to_ak_list(df: pd.DataFrame) -> list[dict]:
+    return [
+        {
+            "info": {
+                "name": row["AK"],
+                "description": row["AKS Kurzbeschreibung"],
+                "head": "" if pd.isna(row["Leitung"]) else row["Leitung"],
+                "reso": "Reso" in row["AKS Typ"].split(","),
+            }
+        }
+        for ak_idx, row in df.iterrows()
+    ]
+
+
+def convert_wiki_csv(wiki_csv_file: str = "result.csv", poll_name: str = "koma-draft"):
+    # download csv from wiki export
+    df = pd.read_csv(wiki_csv_file)
+    ak_list = df_to_ak_list(df)
+
+    write_ak_data(poll_name=poll_name, data={"aks": ak_list})
+
+
+if __name__ == "__main__":
+    fire.Fire(convert_wiki_csv)
