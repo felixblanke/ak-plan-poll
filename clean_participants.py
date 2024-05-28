f6b611f8b2165c2c1af90d1eee518ca502fda6a2
Felix Blanke <info@fblanke.de>
2024-05-29 01:02:00 +0200

    Fix id type

--- clean_participants.py
+++ clean_participants.py
@@ -6,9 +6,25 @@ import fire
 
 from wsgi import get_export_dir, read_ak_data, write_ak_data
 
+def fix_id_type(
+	data_lst: list[dict],
+	clz=str,
+	id_key: str = "id",
+) -> None:
+        for entry in data_lst:
+            if id_key in entry and not isinstance(entry[id_key], clz):
+                entry[id_key] = clz(entry[id_key])
+
 
 def clean_data(poll_name: str, default_duration: int = 2) -> None:
     data = read_ak_data(poll_name)
+    fix_id_type(data["rooms"])
+    fix_id_type(data["timeslots"]["blocks"])
+    fix_id_type(data["aks"])
+
+    if "info" not in data:
+        data["info"] = {}
+
     for idx, ak in enumerate(data["aks"]):
         if "required_time_constraints" in ak:
             ak["time_constraints"] = ak["required_time_constraints"]
@@ -35,6 +51,8 @@ def clean_participants(poll_name: str) -> None:
     for result_json in get_export_dir(poll_name).glob("*.json"):
         with result_json.open("r") as ff:
             result_dict = json.load(ff)
+
+        fix_id_type(result_dict["preferences"], id_key="ak_id")
 
         if "required_time_constraints" in result_dict.keys():
             result_dict["time_constraints"] = result_dict["required_time_constraints"]
